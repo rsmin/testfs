d6eb69c809285ddfc4de26e9b684deeb51b97b5e
rsmin <mengxiaodong1985@msn.com>
2013-05-15 21:52:18 +0800

    try

--- fscachesim-3.0-new/writeCache.hh
+++ fscachesim-3.0-new/writeCache.hh
@@ -16,7 +16,8 @@ using namespace std;
 #include <stdint.h>
 #endif
 #include "Block.hh"
-
+#include <iostream>
+//
 /**
  * writeCache object that supports insertions an its head and tail ends. A major
  * failing of this class is that it does not perform comprehensive bounds
